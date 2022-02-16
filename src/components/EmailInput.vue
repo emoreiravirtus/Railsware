a7e3d041e3ae869b7abd8e5c25f2ebc76c9126b6
Elias Moreira <emoreiravirtus@gmail.com>
2022-02-16 20:02:03 -0300

    Adds EmailInput component

---
+++ src/components/EmailInput.vue
@@ -0,0 +1,80 @@
+<template>
+  <div 
+    class="container p-m border-primary border-s"
+    :class="{'border-error': !valid }"
+    @click="focus">
+    <div class="row">
+      <div class="col-1">
+      <div class="svg-icon-contacts-primary" 
+      :class="{'svg-icon-contacts-error': !valid }">
+      </div>
+      </div>
+      <div class="col-11 line-s">
+        <input 
+          class="font-primary font-m-m"
+          :class="{'font-error': !valid }"
+          ref="emailInput" 
+          type="email" 
+          name="email"
+          placeholder="example@email.com"
+          v-model="email"
+          @change="emitEmail">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmailInfput',
+  data() {
+    return {
+      valid: true,
+      email: ''
+    }
+  },
+  methods: {
+    focus() {
+      this.$refs.emailInput.focus();
+    },
+    emitEmail() {
+
+      if (!this.email) {
+        this.valid = true;
+        this.$emit('emitEmail', { value: '', valid: false });
+      }
+      //eslint-disable-next-line
+      else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
+        this.valid = true;
+        this.$emit('emitEmail', { value: this.email, valid: true });
+      }
+      else {
+        this.valid = false;
+        this.$emit('emitEmail', { value: this.email, valid: false });
+      }
+    }
+
+  }
+}
+</script>
+
+<style lang="scss">
+  .container {
+    max-width: 344px;
+  }
+  input {
+    border: none;
+    width: 100%;
+    transition: $fast-transition;
+
+    &:focus-visible {
+      transform: scale(1.2);
+      outline: none;
+      width: calc(100% - 37px);
+    }
+
+    &::placeholder {
+      color: $primary-color;
+    }
+  }
+</style>
---
+++ src/views/UILibrary.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="font-primary font-center">Ui Library</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2 class="font-secondary font-left">Email Input</h2>
+      </div>
+      <div class="col-12">
+        <EmailInput @emitEmail="handleEmitEmail"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import EmailInput from '@/components/EmailInput'
+
+export default {
+  components: {
+    EmailInput
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
