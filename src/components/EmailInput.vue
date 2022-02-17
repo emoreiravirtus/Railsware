e88c5c0cf2d80ab312e01695d1fad86dcf2b81ec
Elias Moreira <emoreiravirtus@gmail.com>
2022-02-16 21:35:51 -0300

    Adds Button Component

--- src/components/EmailInput.vue
+++ src/components/EmailInput.vue
@@ -58,7 +58,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
   .container {
     max-width: 344px;
   }
--- src/views/UILibrary.vue
+++ src/views/UILibrary.vue
@@ -2,12 +2,12 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1 class="font-primary font-center">Ui Library</h1>
+        <h1 class="font-primary font-center font-l-l line-xl">Ui Library</h1>
       </div>
     </div>
     <div class="row">
       <div class="col-12">
-        <h2 class="font-secondary font-left">Email Input</h2>
+        <h2 class="font-secondary font-center">Email Input</h2>
       </div>
       <div class="col-12">
         <EmailInput @emit="handleEmit"/>
@@ -15,10 +15,40 @@
     </div>
     <div class="row">
       <div class="col-12">
-        <h2 class="font-secondary font-left">Password Input</h2>
+        <h2 class="font-secondary font-center">Password Input</h2>
       </div>
       <div class="col-12">
         <PasswordInput @emit="handleEmit"/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2 class="font-secondary font-center">Buttons</h2>
+      </div>
+      <div class="col-12">
+        <Button 
+          :size="'big'"
+          :actionText="'Log in'"
+          @clicked="handleEmit"/>
+      </div>
+      <div class="col-12 mt-s">
+        <Button 
+          :backgroundColor="'secondary'"
+          :size="'big'"
+          :actionText="'Upgrade'"
+          @clicked="handleEmit"/>
+      </div>
+      <div class="col-6 mt-s">
+        <Button 
+          :backgroundColor="'google'"
+          :actionText="'Google'"
+          @clicked="handleEmit"/>
+      </div>
+      <div class="col-6 mt-s">
+        <Button 
+          :backgroundColor="'facebook'"
+          :actionText="'Facebook'"
+          @clicked="handleEmit"/>
       </div>
     </div>
   </div>
@@ -27,12 +57,13 @@
 <script>
 import EmailInput from '@/components/EmailInput'
 import PasswordInput from '@/components/PasswordInput'
-
+import Button from '@/components/Button'
 
 export default {
   components: {
     EmailInput,
-    PasswordInput
+    PasswordInput,
+    Button
   },
   methods: {
     handleEmit(e){
